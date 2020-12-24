3d139435bdbb52bfd066ec8711d8b11021433e1b
egorzainullin <egor.zainullin@icloud.com>
2020-12-24 08:08:46 +0300

    -fixed attributes

--- Repo.Source/LanguageMetamodel/Details/Elements/LanguageElement.fs
+++ Repo.Source/LanguageMetamodel/Details/Elements/LanguageElement.fs
@@ -31,7 +31,7 @@ type LanguageElement(element: IAttributeElement, pool: LanguagePool, repo: IAttr
         languageMetamodel.Node Repo.LanguageMetamodel.Consts.association
 
     let attributeMetamodel = repo.Model Consts.attributeMetamodel
-    
+
     let attributeMetatype = attributeMetamodel.Node Consts.attribute
 
     let attributesAssociationMetatype =
@@ -45,18 +45,18 @@ type LanguageElement(element: IAttributeElement, pool: LanguagePool, repo: IAttr
 
     let slotsAssociationMetatype =
         attributeMetamodel.Association Consts.slotsEdge
-      
+
 
     let wrap = pool.Wrap
-   
+
     let model () = element.Model
-    
-    let mutable AttributeMap = Map.empty: Map<string, IAttributeElement>
+
+    let mutable AttributeMap: Map<string, IAttributeElement> = Map.empty
 
     let unwrap (element: ILanguageElement) =
         (element :?> LanguageElement).UnderlyingElement
 
-    /// Instantiation of association with given metatype. 
+    /// Instantiation of association with given metatype.
     let (--->) source (target, metatype) =
         (model ()).InstantiateAssociation source target metatype
         |> ignore
@@ -77,8 +77,7 @@ type LanguageElement(element: IAttributeElement, pool: LanguagePool, repo: IAttr
             element.OutgoingEdges
             |> Seq.map pool.Wrap
             |> Seq.cast<ILanguageEdge>
-        
-        
+
         member this.OutgoingAssociations =
             element.OutgoingAssociations
             |> Seq.map pool.Wrap
@@ -99,32 +98,16 @@ type LanguageElement(element: IAttributeElement, pool: LanguagePool, repo: IAttr
             |> Seq.map wrap
 
         member this.Attributes =
-            AttributeMap
-            |> Map.toSeq
-            |> Seq.map (fun (_, node) -> node)
-            |> Seq.map (fun node -> node.Attributes)
-            |> Seq.concat
-            |> Seq.map (pool.WrapAttribute)
-              
-             
-        member this.AddAttribute name ``type`` =
-            if (this :> ILanguageElement).Attributes
-               |> Seq.filter (fun a -> a.Name = name)
-               |> Seq.length = 1 then
-                raise <| AmbiguousAttributesException(name)
-            let attributeNode = (model ()).InstantiateNode name attributeMetatype AttributeMap
-            attributeNode
-            ---> (unwrap ``type``, typeAssociationMetatype)
-            element
-            ---> (attributeNode, attributesAssociationMetatype)
-            AttributeMap <- AttributeMap.Add(name, attributeNode)
+            element.Attributes |> Seq.map (pool.WrapAttribute)
 
-        member this.Slots =
-            element.Slots
-            |> Seq.map pool.WrapSlot
-            
+        member this.AddAttribute name ``type`` =
+            element.AddAttribute name (unwrap ``type``)
+
+        member this.Slots = element.Slots |> Seq.map pool.WrapSlot
+
         member this.Slot name =
-            (this :> ILanguageElement).Slots
+            let slots = (this :> ILanguageElement).Slots
+            slots
             |> Seq.filter (fun s -> s.Attribute.Name = name)
             |> Helpers.exactlyOneElement name
 
