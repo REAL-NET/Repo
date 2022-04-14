85217bcf48dc19f0549c2ee6eb4e28f54ee804f1
Yuniya Kim <kim.yuniya@mail.ru>
2022-04-14 14:49:05 +0300

    Fix attributes

--- Repo.Source/ModelBuilders/QueryMetamodelBuilder.fs
+++ Repo.Source/ModelBuilders/QueryMetamodelBuilder.fs
@@ -33,11 +33,11 @@ type QueryMetamodelBuilder() =
 
             let model = repo.CreateModel("QueryMetamodel", metamodel)
 
-            let (~+) (name, xCoordinate, yCoordinate) =
+            let (~+) (name) =
                 let node = infrastructure.Instantiate model metamodelNode :?> INode
                 node.Name <- name
-                infrastructure.Element.SetAttributeValue node "xCoordinate" xCoordinate
-                infrastructure.Element.SetAttributeValue node "yCoordinate" yCoordinate
+                infrastructure.Element.AddAttribute node "xCoordinate" "AttributeKind.Int" "0"
+                infrastructure.Element.AddAttribute node "yCoordinate" "AttributeKind.Int" "0"
 
                 node
 
@@ -50,34 +50,35 @@ type QueryMetamodelBuilder() =
                 edge.Target <- Some target
                 edge.TargetName <- targetName
 
+                infrastructure.Element.AddAttribute edge "connectionType" "AttributeKind.String" ""
                 infrastructure.Element.SetAttributeValue edge "connectionType" "local"
                 infrastructure.Element.SetAttributeValue edge "name" linkName
 
                 edge
 
-            let abstractQueryBlock  = +("AbstractQueryBlock", "", "")
+            let abstractQueryBlock  = +("AbstractQueryBlock")
 
-            let operator = +("Operator", "", "")
+            let operator = +("Operator")
             infrastructure.Element.AddAttribute operator "children" "AttributeKind.String" ""
             infrastructure.Element.AddAttribute operator "parent" "AttributeKind.String" ""
             infrastructure.Element.AddAttribute operator "connectionType" "AttributeKind.String" ""
 
-            let operatorInternals = +("OperatorInternals", "", "")
+            let operatorInternals = +("OperatorInternals")
             infrastructure.Element.AddAttribute operatorInternals "contents" "AttributeKind.String" ""
 
-            let reader = +("Reader", "", "")
+            let reader = +("Reader")
             infrastructure.Element.AddAttribute reader "parent" "AttributeKind.String" ""
             infrastructure.Element.AddAttribute reader "connectionType" "AttributeKind.String" ""
             infrastructure.Element.AddAttribute reader "argument" "AttributeKind.String" ""
             
-            let ds = +("DS", "", "")
+            let ds = +("DS")
             infrastructure.Element.AddAttribute ds "argument" "AttributeKind.String" ""
-            let sort = +("Sort", "", "")
-            let join = +("Join", "", "")
-            let aggregate = +("Aggregate", "", "")
-            let filter = +("Filter", "", "")
-            let materialize = +("Materialize", "", "")
-            let read = +("Read", "", "")
+            let sort = +("Sort")
+            let join = +("Join")
+            let aggregate = +("Aggregate")
+            let filter = +("Filter")
+            let materialize = +("Materialize")
+            let read = +("Read")
 
             let link = abstractQueryBlock ---> (abstractQueryBlock, "target", "Link")
 
