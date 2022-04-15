11d3daf00aa0dcdca57668b35fbdc75dad024d5a
Yuniya Kim <kim.yuniya@mail.ru>
2022-04-15 14:05:56 +0300

    Add more query model tests

--- Repo.Tests/QueryModelTests.fs
+++ Repo.Tests/QueryModelTests.fs
@@ -9,7 +9,17 @@ let repo =
     RepoFactory.Create ()
 
 [<Test>]
-let ``Metamodel and model exist`` () =
+let ``Metamodel and model should exist`` () =
     let metamodel = repo.Model "QueryMetamodel"
     let model = repo.Model "QueryModel"
     model.Metamodel |> should equal metamodel
+
+[<Test>]
+let ``Model should have 7 nodes`` () =
+    let model = repo.Model "QueryModel"
+    model.Metamodel.Nodes |> Seq.length |> should equal 7
+
+[<Test>]
+let ``Model should have 4 edges`` () =
+    let model = repo.Model "QueryModel"
+    model.Metamodel.Edges |> Seq.length |> should equal 4
