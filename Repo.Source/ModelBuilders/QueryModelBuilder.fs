483cfb32f54ccaf59b364fe9b00a2e57485d5c0d
Yuniya Kim <kim.yuniya@mail.ru>
2022-05-03 13:22:30 +0300

    Clear query model

--- Repo.Source/ModelBuilders/QueryModelBuilder.fs
+++ Repo.Source/ModelBuilders/QueryModelBuilder.fs
@@ -26,93 +26,93 @@ type QueryModelBuilder() =
             let infrastructure = InfrastructureSemantic(repo)
             let metamodel = Repo.findModel repo "QueryMetamodel"
 
-            let metamodelAbstractQueryBlock = Model.findNode metamodel "AbstractQueryBlock"
-            let metamodelMaterializationLine = Model.findNode metamodel "MaterializationLine"
+            //let metamodelAbstractQueryBlock = Model.findNode metamodel "AbstractQueryBlock"
+            //let metamodelMaterializationLine = Model.findNode metamodel "MaterializationLine"
 
-            let findNodeWithAttribute nodes attributeName attributeValue =
-                let node = nodes |> Seq.filter (fun m -> infrastructure.Element.AttributeValue m attributeName = attributeValue)
-                if Seq.isEmpty node then
-                    raise (InvalidSemanticOperationException <| sprintf "Node with attribute %s value %s not found" attributeName attributeValue)
-                elif Seq.length node <> 1 then
-                    raise (InvalidSemanticOperationException
-                        <| sprintf "Node with attribute %s value %s appears more than once" attributeName attributeValue)
-                else
-                    Seq.head node
+            //let findNodeWithAttribute nodes attributeName attributeValue =
+            //    let node = nodes |> Seq.filter (fun m -> infrastructure.Element.AttributeValue m attributeName = attributeValue)
+            //    if Seq.isEmpty node then
+            //        raise (InvalidSemanticOperationException <| sprintf "Node with attribute %s value %s not found" attributeName attributeValue)
+            //    elif Seq.length node <> 1 then
+            //        raise (InvalidSemanticOperationException
+            //            <| sprintf "Node with attribute %s value %s appears more than once" attributeName attributeValue)
+            //    else
+            //        Seq.head node
 
-            let metamodelSorts = Model.findNodes metamodel "Sort"
-            let metamodelSort = findNodeWithAttribute metamodelSorts "type" "tuple"
-            let metamodelAggregate = Model.findNode metamodel "Aggregate"
-            let metamodelJoins = Model.findNodes metamodel "Join"
-            let metamodelJoin = findNodeWithAttribute metamodelJoins "type" "positional"
-            let metamodelRead = Model.findNode metamodel "Read"
-            let metamodelOperatorInternals = Model.findNode metamodel "OperatorInternals"
+            //let metamodelSorts = Model.findNodes metamodel "Sort"
+            //let metamodelSort = findNodeWithAttribute metamodelSorts "type" "tuple"
+            //let metamodelAggregate = Model.findNode metamodel "Aggregate"
+            //let metamodelJoins = Model.findNodes metamodel "Join"
+            //let metamodelJoin = findNodeWithAttribute metamodelJoins "type" "positional"
+            //let metamodelRead = Model.findNode metamodel "Read"
+            //let metamodelOperatorInternals = Model.findNode metamodel "OperatorInternals"
 
-            let link = Model.findAssociationWithSource metamodelAbstractQueryBlock "target"
+            //let link = Model.findAssociationWithSource metamodelAbstractQueryBlock "target"
 
-            let model = repo.CreateModel("QueryModel", metamodel)
+            //let model = repo.CreateModel("QueryModel", metamodel)
 
-            let materializationLine = infrastructure.Instantiate model metamodelMaterializationLine
-            infrastructure.Element.SetAttributeValue materializationLine "xCoordinate" "100"
-            infrastructure.Element.SetAttributeValue materializationLine "yCoordinate" "220"
+            //let materializationLine = infrastructure.Instantiate model metamodelMaterializationLine
+            //infrastructure.Element.SetAttributeValue materializationLine "xCoordinate" "100"
+            //infrastructure.Element.SetAttributeValue materializationLine "yCoordinate" "220"
 
-            let sort = infrastructure.Instantiate model metamodelSort
-            infrastructure.Element.SetAttributeValue sort "xCoordinate" "235"
-            infrastructure.Element.SetAttributeValue sort "yCoordinate" "70"
+            //let sort = infrastructure.Instantiate model metamodelSort
+            //infrastructure.Element.SetAttributeValue sort "xCoordinate" "235"
+            //infrastructure.Element.SetAttributeValue sort "yCoordinate" "70"
 
-            let operatorInternals1 = infrastructure.Instantiate model metamodelOperatorInternals
-            infrastructure.Element.SetAttributeValue operatorInternals1 "xCoordinate" "100"
-            infrastructure.Element.SetAttributeValue operatorInternals1 "yCoordinate" "120"
+            //let operatorInternals1 = infrastructure.Instantiate model metamodelOperatorInternals
+            //infrastructure.Element.SetAttributeValue operatorInternals1 "xCoordinate" "100"
+            //infrastructure.Element.SetAttributeValue operatorInternals1 "yCoordinate" "120"
 
-            let aggregate = infrastructure.Instantiate model metamodelAggregate
-            infrastructure.Element.SetAttributeValue aggregate "xCoordinate" "135"
-            infrastructure.Element.SetAttributeValue aggregate "yCoordinate" "25"
+            //let aggregate = infrastructure.Instantiate model metamodelAggregate
+            //infrastructure.Element.SetAttributeValue aggregate "xCoordinate" "135"
+            //infrastructure.Element.SetAttributeValue aggregate "yCoordinate" "25"
 
-            let operatorInternals2 = infrastructure.Instantiate model metamodelOperatorInternals
-            infrastructure.Element.SetAttributeValue operatorInternals2 "xCoordinate" "100"
-            infrastructure.Element.SetAttributeValue operatorInternals2 "yCoordinate" "240"
+            //let operatorInternals2 = infrastructure.Instantiate model metamodelOperatorInternals
+            //infrastructure.Element.SetAttributeValue operatorInternals2 "xCoordinate" "100"
+            //infrastructure.Element.SetAttributeValue operatorInternals2 "yCoordinate" "240"
 
-            let join = infrastructure.Instantiate model metamodelJoin
-            infrastructure.Element.SetAttributeValue join "xCoordinate" "135"
-            infrastructure.Element.SetAttributeValue join "yCoordinate" "25"
+            //let join = infrastructure.Instantiate model metamodelJoin
+            //infrastructure.Element.SetAttributeValue join "xCoordinate" "135"
+            //infrastructure.Element.SetAttributeValue join "yCoordinate" "25"
 
-            let read1 = infrastructure.Instantiate model metamodelRead
-            infrastructure.Element.SetAttributeValue read1 "xCoordinate" "30"
-            infrastructure.Element.SetAttributeValue read1 "yCoordinate" "25"
-            infrastructure.Element.SetAttributeValue read1 "argument" "d_datekey"
+            //let read1 = infrastructure.Instantiate model metamodelRead
+            //infrastructure.Element.SetAttributeValue read1 "xCoordinate" "30"
+            //infrastructure.Element.SetAttributeValue read1 "yCoordinate" "25"
+            //infrastructure.Element.SetAttributeValue read1 "argument" "d_datekey"
 
-            let read2 = infrastructure.Instantiate model metamodelRead
-            infrastructure.Element.SetAttributeValue read2 "xCoordinate" "240"
-            infrastructure.Element.SetAttributeValue read2 "yCoordinate" "25"
-            infrastructure.Element.SetAttributeValue read2 "argument" "lo_orderdate"
+            //let read2 = infrastructure.Instantiate model metamodelRead
+            //infrastructure.Element.SetAttributeValue read2 "xCoordinate" "240"
+            //infrastructure.Element.SetAttributeValue read2 "yCoordinate" "25"
+            //infrastructure.Element.SetAttributeValue read2 "argument" "lo_orderdate"
 
-            let (-->) (src: IElement) dst =
-                let aLink = infrastructure.Instantiate model link :?> IAssociation
-                aLink.Source <- Some src
-                aLink.Target <- Some dst
-                aLink
+            //let (-->) (src: IElement) dst =
+            //    let aLink = infrastructure.Instantiate model link :?> IAssociation
+            //    aLink.Source <- Some src
+            //    aLink.Target <- Some dst
+            //    aLink
 
-            let edgeToAggregate = operatorInternals1 --> aggregate 
-            infrastructure.Element.SetAttributeValue edgeToAggregate "type" "internals"
+            //let edgeToAggregate = operatorInternals1 --> aggregate 
+            //infrastructure.Element.SetAttributeValue edgeToAggregate "type" "internals"
 
-            let edgeToJoin = operatorInternals2 --> join
-            infrastructure.Element.SetAttributeValue edgeToJoin "type" "internals"
+            //let edgeToJoin = operatorInternals2 --> join
+            //infrastructure.Element.SetAttributeValue edgeToJoin "type" "internals"
 
-            let edgeToRead1 = operatorInternals2 --> read1
-            infrastructure.Element.SetAttributeValue edgeToRead1 "type" "internals"
+            //let edgeToRead1 = operatorInternals2 --> read1
+            //infrastructure.Element.SetAttributeValue edgeToRead1 "type" "internals"
 
-            let edgeToRead2 = operatorInternals2 --> read2
-            infrastructure.Element.SetAttributeValue edgeToRead2 "type" "internals"
+            //let edgeToRead2 = operatorInternals2 --> read2
+            //infrastructure.Element.SetAttributeValue edgeToRead2 "type" "internals"
 
-            let joinToRead1 = join --> read1
-            infrastructure.Element.SetAttributeValue joinToRead1 "type" "remote"
-            infrastructure.Element.SetAttributeValue joinToRead1 "sourcePort" "left"
-            infrastructure.Element.SetAttributeValue joinToRead1 "targetPort" "right"
+            //let joinToRead1 = join --> read1
+            //infrastructure.Element.SetAttributeValue joinToRead1 "type" "remote"
+            //infrastructure.Element.SetAttributeValue joinToRead1 "sourcePort" "left"
+            //infrastructure.Element.SetAttributeValue joinToRead1 "targetPort" "right"
 
-            sort --> aggregate |> ignore
-            aggregate --> join |> ignore
+            //sort --> aggregate |> ignore
+            //aggregate --> join |> ignore
 
-            let joinToRead2 = join --> read2
-            infrastructure.Element.SetAttributeValue joinToRead2 "sourcePort" "right"
-            infrastructure.Element.SetAttributeValue joinToRead2 "targetPort" "left"
+            //let joinToRead2 = join --> read2
+            //infrastructure.Element.SetAttributeValue joinToRead2 "sourcePort" "right"
+            //infrastructure.Element.SetAttributeValue joinToRead2 "targetPort" "left"
 
             ()
